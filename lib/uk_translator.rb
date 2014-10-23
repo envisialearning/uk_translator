448e08f2e7ba145a6e39e9aa75f7103800167664
Aldwin Ibuna <aldwin@apemangroup.com>
2014-10-23 11:53:01 +0800

    Updated match pattern to only replace whole words.
    Updated match pattern to only replace whole words.

--- lib/uk_translator.rb
+++ lib/uk_translator.rb
@@ -11,9 +11,9 @@ module UkTranslator
   def self.us_to_uk(us_string)
     uk_string = us_string
     self.data.each do |us_word, uk_word|
-      uk_string.gsub!(us_word, uk_word)
-      uk_string.gsub!(us_word.capitalize, uk_word.capitalize)
-      uk_string.gsub!(us_word.upcase, uk_word.upcase)
+      uk_string.gsub!(/#{'\b'+us_word+'\b'}/, uk_word)
+      uk_string.gsub!(/#{'\b'+us_word.capitalize+'\b'}/, uk_word.capitalize)
+      uk_string.gsub!(/#{'\b'+us_word.upcase+'\b'}/, uk_word.upcase)
     end
     uk_string
   end
@@ -21,9 +21,9 @@ module UkTranslator
   def self.uk_to_us(uk_string)
     us_string = uk_string
     self.data.each do |us_word, uk_word|
-      us_string.gsub!(uk_word, us_word)
-      us_string.gsub!(uk_word.capitalize, us_word.capitalize)
-      us_string.gsub!(uk_word.upcase, us_word.upcase)
+      us_string.gsub!(/#{'\b'+uk_word+'\b'}/, us_word)
+      us_string.gsub!(/#{'\b'+uk_word.capitalize+'\b'}/, us_word.capitalize)
+      us_string.gsub!(/#{'\b'+uk_word.upcase+'\b'}/, us_word.upcase)
     end
     us_string
   end
